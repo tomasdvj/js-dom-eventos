8f2534256dd3454a19aa4de38416b15e3dc8a2d5
Tomas Vergara <tomas.d.v.j@hotmail.com>
2022-10-17 12:35:43 -0300

    querySelectorAll y getElementById agregados

--- js/app.js
+++ js/app.js
@@ -6,7 +6,18 @@ console.log(heading);
 
 
 //! querySelectorAll
+// const enlaces = document.querySelectorAll('.navegacion a'); //! Retorna todos los elementos que concurden con el selector
+// enlaces[0].textContent = 'Nuevo texto para enlace'; //! Se modifica elemento apuntando a la posicion del arreglo
 
+//! Nomenclatura valida pero poco usada
+const enlaces = document.querySelectorAll('.navegacion a')[0].textContent = 'Nuevo texto para enlace'; //! Se puede usar una sola vez
+const enlaces2 = document.querySelectorAll('.navegacion a');
+enlaces2[0].href = 'https://www.google.com';
+enlaces2[0].classList.add('nueva-clase');
+
+console.log(enlaces);
 
 
 //! getElementById
+const heading2 = document.getElementById('heading');
+console.log(heading2);
