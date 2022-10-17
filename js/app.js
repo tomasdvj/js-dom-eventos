bdab3e4f77f5eb99e0e7e9bc9941276214f4a826
Tomas Vergara <tomas.d.v.j@hotmail.com>
2022-10-17 15:45:08 -0300

    Generando codigo HTML en JS

--- js/app.js
+++ js/app.js
@@ -9,11 +9,14 @@ console.log(heading);
 // const enlaces = document.querySelectorAll('.navegacion a'); //! Retorna todos los elementos que concurden con el selector
 // enlaces[0].textContent = 'Nuevo texto para enlace'; //! Se modifica elemento apuntando a la posicion del arreglo
 
-//! Nomenclatura valida pero poco usada
-const enlaces = document.querySelectorAll('.navegacion a')[0].textContent = 'Nuevo texto para enlace'; //! Se puede usar una sola vez
-const enlaces2 = document.querySelectorAll('.navegacion a');
-enlaces2[0].href = 'https://www.google.com';
-enlaces2[0].classList.add('nueva-clase');
+//! Nomenclatura valida pero genera problemas
+// const enlaces = document.querySelectorAll('.navegacion a')[0].textContent = 'Nuevo texto para enlace';
+
+//! Lo que se recomienda usar
+const enlaces = document.querySelectorAll('.navegacion a');
+enlaces[0].textContent = 'Nuevo texto para enlace';
+enlaces[0].href = 'https://www.google.com';
+enlaces[0].classList.add('nueva-clase');
 
 console.log(enlaces);
 
@@ -21,3 +24,21 @@ console.log(enlaces);
 //! getElementById
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+//! Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A') //! JS recomienda crear las etiquetas en mayusculas y sin aperturas
+
+//! Agregar href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+//! Agregar texto
+nuevoEnlace.textContent = 'Un nuevo enlace';
+
+//! Agregar clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//! Agregar al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
