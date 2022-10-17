859fc8defef5052d8a7784875d23024c178ea4b0
Tomas Vergara <tomas.d.v.j@hotmail.com>
2022-10-17 12:18:32 -0300

    Se agrega querySelector

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//! querySelector
+// const heading = document.querySelector('.header__texto h2'); //! Retorna 0 o 1 elementos
+const heading = document.querySelector('#heading'); //! Se puede seleccionar elementos por id con #
+heading.textContent = 'Nuevo heading'; //! Se puede modificar mediante JS
+console.log(heading);
+
+
+//! querySelectorAll
+
+
+
+//! getElementById
